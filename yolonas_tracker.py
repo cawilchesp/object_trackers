55f613233df547b5a7e5dfa0a52de0a43b26ff7f
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-12-13 17:59:49 -0500

    Update yolonas_tracker.py

--- yolonas_tracker.py
+++ yolonas_tracker.py
@@ -126,7 +126,7 @@ def main():
 
 if __name__ == "__main__":
     # Initialize Configuration File
-    with open('config.yaml', 'r') as file:
+    with open('trackers_config.yaml', 'r') as file:
         config = yaml.safe_load(file)
 
     # Get configuration parameters
