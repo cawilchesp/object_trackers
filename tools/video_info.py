99466ecc9f2bbe84689350d0840ba9444578ef51
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2024-01-22 11:49:01 -0500

    Create video_info.py

---
+++ tools/video_info.py
@@ -0,0 +1,28 @@
+import cv2
+
+
+class VideoInfo:
+    def __init__(self, width: int = 0, height: int = 0, fps: int = 0, total_frames: float = None) -> None:
+        self.width = width
+        self.height = height
+        self.fps = fps
+        self.total_frames = total_frames
+
+
+def from_video_path(source_cap: cv2.VideoCapture) -> VideoInfo:
+    if source_cap.isOpened():
+        width = int(source_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(source_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = source_cap.get(cv2.CAP_PROP_FPS)
+        total_frames = int(source_cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    return VideoInfo(width, height, fps, total_frames)
+
+
+def from_camera(source_cap: cv2.VideoCapture) -> VideoInfo:
+        if source_cap.isOpened():
+            width = int(source_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+            height = int(source_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            fps = source_cap.get(cv2.CAP_PROP_FPS)
+
+        return VideoInfo(width, height, fps)
