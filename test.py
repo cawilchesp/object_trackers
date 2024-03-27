d8f1892e31b40940112b9b66996b45082474a6a3
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2024-03-26 19:29:21 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,19 @@
 from detector import ObjectDetection
 
-detector = ObjectDetection(
-    source="D:\Data\C3_detenido.mp4",
-    weights='yolov9c.pt')
-detector()
+def main():
+    detector = ObjectDetection(
+        source='D:\Data\C3_detenido.mp4',
+        # source=0,
+        weights='yolov8m',
+        # weights='yolov9c',
+        mode='track',
+        labels=True,
+        boxes=True,
+        tracks=True,
+        show_fps=True,
+        # class_filter=[2],
+        output='D:\Data\C3_detenido' )
+    detector()
+
+if __name__ == "__main__":
+    main()
