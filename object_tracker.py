6eaddb72d9d2fe8e0f94b2260ed50f26f30b42d1
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2024-04-06 20:29:21 -0500

    Create object_tracker.py

---
+++ object_tracker.py
@@ -0,0 +1,161 @@
+from ultralytics import YOLO
+import supervision as sv
+
+import imutils
+from imutils.video import FileVideoStream
+from imutils.video import FPS
+
+import cv2
+import yaml
+import torch
+import time
+from tqdm import tqdm
+from pathlib import Path
+
+from tools.print_info import print_video_info, print_progress, step_message
+from tools.write_csv import output_data_list, write_csv
+from tools.video_info import from_video_path
+from tools.csv_sink import CSVSink
+
+# For debugging
+from icecream import ic
+
+
+def time_synchronized():
+    """ PyTorch accurate time """
+    if torch.cuda.is_available(): torch.cuda.synchronize()
+    return time.time()
+
+
+def process_step() -> str:
+    """ Simple step counter """
+    global step_count
+    step_count = str(int(step_count) + 1)
+    return step_count
+
+
+def main(
+        source: str,
+        output: str,
+        weights: str,
+        class_filter: list[int],
+        image_size: int,
+        confidence: float,
+        track_length: int,
+        show_image: bool,
+        save_csv: bool,
+        save_video: bool
+    ) -> None:
+
+    # Initialize YOLOv8 Model
+    model = YOLO(weights)
+    step_message(process_step(), 'Model Initialized')
+
+    # Inicializar captura de video
+    cap = cv2.VideoCapture(source)
+    if not cap.isOpened(): quit()
+    source_info = from_video_path(cap)
+    print_video_info(source, source_info)
+    
+    # Anotatores
+    line_thickness = int(sv.calculate_dynamic_line_thickness(resolution_wh=(source_info.width, source_info.height)) * 0.5)
+    text_scale = sv.calculate_dynamic_text_scale(resolution_wh=(source_info.width, source_info.height)) * 0.5
+
+    label_annotator = sv.LabelAnnotator(text_scale=text_scale, text_padding=2, text_position=sv.Position.TOP_LEFT, text_thickness=line_thickness)
+    bounding_box_annotator = sv.BoundingBoxAnnotator(thickness=line_thickness)
+    trace_annotator = sv.TraceAnnotator(position=sv.Position.CENTER, trace_length=track_length, thickness=line_thickness)
+
+    # Iniciar procesamiento de video
+    step_message(process_step(), 'Procesamiento de video iniciado')
+    fvs = FileVideoStream(source).start()
+    video_sink = sv.VideoSink(target_path=f"{output}.mp4", video_info=source_info, codec="mp4v")
+    csv_sink = CSVSink(file_name=f"{output}.csv")
+
+    frame_number = 0
+    with video_sink, csv_sink:
+        t_frame_start = time_synchronized()
+        while fvs.more():
+            
+            image = fvs.read()
+            annotated_image = image.copy()
+            
+            # Process YOLOv8 detections
+            results = model.track(
+                source=image,
+                persist=True,
+                imgsz=image_size,
+                conf=confidence,
+                device='cuda' if torch.cuda.is_available() else 'cpu',
+                classes=class_filter,
+                retina_masks=True,
+                verbose=False
+            )[0]
+            detections = sv.Detections.from_ultralytics(results)
+            detections = detections.with_nms()
+
+            if show_image or save_video:
+                # Dibujar etiquetas
+                object_labels = [f"{data['class_name']} {tracker_id} ({score:.2f})" for _, _, score, _, tracker_id, data in detections]
+                annotated_image = label_annotator.annotate(
+                    scene=annotated_image,
+                    detections=detections,
+                    labels=object_labels )
+
+                # Draw boxes
+                annotated_image = bounding_box_annotator.annotate(
+                    scene=annotated_image,
+                    detections=detections )
+                
+                # Draw tracks
+                annotated_image = trace_annotator.annotate(
+                    scene=annotated_image,
+                    detections=detections )
+                
+            if save_video: video_sink.write_frame(frame=annotated_image)
+            if save_csv: csv_sink.append(detections, custom_data={'frame_number': frame_number})
+
+            frame_number += 1
+
+            if show_image:
+                cv2.imshow("Resultado", annotated_image)
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    break
+
+    t_frame_end = time_synchronized()
+    print(f"Time: {t_frame_end - t_frame_start} s / frames: {frame_number}")
+    cv2.destroyAllWindows()
+    fvs.stop()
+
+
+if __name__ == "__main__":
+    # Initialize Configuration File
+    with open('tracking_config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+
+    # Get configuration parameters
+    MODEL_FOLDER = config['MODEL']['YOLOV8_FOLDER']
+    MODEL_WEIGHTS = config['MODEL']['YOLOV8_WEIGHTS']
+    FOLDER = config['INPUT']['FOLDER']
+    SOURCE = config['INPUT']['SOURCE']
+    CLASS_FILTER = config['DETECTION']['CLASS_FILTER']
+    IMAGE_SIZE = config['DETECTION']['IMAGE_SIZE']
+    CONFIDENCE = config['DETECTION']['CONFIDENCE']
+    TRACK_LENGTH = config['DRAW']['TRACK_LENGTH']
+    SHOW_RESULTS = config['SHOW']
+    SAVE_VIDEO = config['SAVE']['VIDEO']
+    SAVE_CSV = config['SAVE']['CSV']
+
+    step_count = '0'
+    
+    main(
+        source=f"{FOLDER}/{SOURCE}",
+        output=f"{FOLDER}/{Path(SOURCE).stem}_tracking",
+        weights=f"{MODEL_FOLDER}/{MODEL_WEIGHTS}.pt",
+        class_filter=CLASS_FILTER,
+        image_size=IMAGE_SIZE,
+        confidence=CONFIDENCE,
+        track_length=TRACK_LENGTH,
+        show_image=SHOW_RESULTS,
+        save_csv=SAVE_CSV,
+        save_video=SAVE_VIDEO
+    )
