9a35699831745f229c9b03f8a7bede9d66160b8e
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2024-04-08 21:35:42 -0500

    Update csv_process.py

--- csv_process.py
+++ csv_process.py
@@ -34,78 +34,92 @@ def combine_csv_files(camera_id, day, hour):
 
 
 def process_csv_file(camera_id, day, hour):
+    # Speed estimation
     ZONE_ANALYSIS = np.array([[279,64], [406,64], [635,338], [0,338]])
     TARGET_WIDTH = 25
-    TARGET_HEIGHT = 100
+    TARGET_HEIGHT = 120
     TARGET = np.array( [ [0, 0], [TARGET_WIDTH - 1, 0], [TARGET_WIDTH - 1, TARGET_HEIGHT - 1], [0, TARGET_HEIGHT - 1] ] )
-
     view_transformer = ViewTransformer(source=ZONE_ANALYSIS, target=TARGET)
+    
+    
+    # Anotatores
+    line_thickness = int(sv.calculate_dynamic_line_thickness(resolution_wh=(704, 480)) * 0.5)
+    text_scale = sv.calculate_dynamic_text_scale(resolution_wh=(704, 480)) * 0.5
 
+    label_annotator = sv.LabelAnnotator(text_scale=text_scale, text_padding=2, text_position=sv.Position.TOP_LEFT, text_thickness=line_thickness)
+    bounding_box_annotator = sv.BoundingBoxAnnotator(thickness=line_thickness)
+    trace_annotator = sv.TraceAnnotator(position=sv.Position.CENTER, trace_length=50, thickness=line_thickness)
+
+    # Line counters
+    line_zone_1 = sv.LineZone(start=sv.Point(x=65, y=220), end=sv.Point(x=118, y=220))
+    line_zone_2 = sv.LineZone(start=sv.Point(x=118, y=220), end=sv.Point(x=173, y=220))
+    
+    id_classes = {
+        'bicycle': 0,
+        'bus': 1,
+        'car': 2,
+        'motorbike': 3,
+        'person': 4,
+        'truck': 5 
+    }
+
+
+    # Load CSV data
     csv_file = f"D:/Data/Piloto_EDGE/CCT_{camera_id}_{day}{hour}/{camera_id}.csv"
+    csv_data = pd.read_csv(csv_file, sep=',', names=['frame', 'id', 'class', 'x', 'y', 'w', 'h', 'score'], header=None, index_col=False)
+    csv_data['x2'] = csv_data['x'] + csv_data['w']
+    csv_data['y2'] = csv_data['y'] + csv_data['h']
+    csv_data['class_id'] = csv_data['class'].replace(id_classes)
+    
+    max_frame = csv_data['frame'].max()
+    
+    # Variables
     time_step = 1.0 / 10.0
 
-    image = np.zeros([480, 704, 3], np.uint8)
-    image = sv.draw_polygon(scene=image, polygon=ZONE_ANALYSIS, color=sv.Color.RED)
-
-    data = pd.read_csv(csv_file, sep=',', names=['frame', 'id', 'class', 'x', 'y', 'w', 'h', 'score'], header=None, index_col=False)
-    
-    object_id_list = data['id'].unique().astype(int)
-
-    object_number = 0
-    for object in object_id_list:
-        print(object_number)
-        object_data = data[data['id'] == object].copy()
-        object_data.reset_index(drop=True, inplace=True)
-        object_data['time_frame'] = object_data['frame'] * time_step
-        object_data['center_x'] = object_data['x'] + (object_data['w'] / 2)
-        object_data['center_y'] = object_data['y'] + object_data['h']
-
-        points = object_data[['center_x', 'center_y']].to_numpy()
-        points = view_transformer.transform_points(points=points).astype(int)
-        points = pd.DataFrame(points, columns=['x_transformed', 'y_transformed'])
-
-        object_data = pd.concat([object_data, points], axis = 1)
-
-        previous_time = 0.0
-        previous_x = 0
-        previous_y = 0
-        speed_column = []
-        for current_time, current_x, current_y in zip(object_data['time_frame'], object_data['x_transformed'], object_data['y_transformed']):
-            distance = abs(np.sqrt((current_y-previous_y)**2 + (current_x-previous_x)**2))
-            time_diff = current_time - previous_time
-
-            speed = distance / time_diff * 3.6 if time_diff != 0.0 else 0.0
-            speed_column.append(speed)
-            previous_time = current_time
-            previous_x = current_x
-            previous_y = current_y
-
-        object_data['speed'] = speed_column
-
-        object_class = object_data['class'][0]
+    frame_number = 0
+    while frame_number < max_frame:
+        annotated_image = np.zeros([480, 704, 3], np.uint8)
+        annotated_image = sv.draw_polygon(scene=annotated_image, polygon=ZONE_ANALYSIS, color=sv.Color.RED)
         
-        object_x = object_data['center_x'].to_list()
-        object_y = object_data['center_y'].to_list()
-        object_speed = object_data['speed'].to_list()[1:]
+        detections_dataframe = csv_data[csv_data['frame'] == frame_number].copy()
+        
+        detections = sv.Detections(
+            xyxy=detections_dataframe[['x', 'y', 'x2', 'y2']].to_numpy().astype(np.float32),
+            mask=None,
+            confidence=detections_dataframe[['score']].to_numpy().ravel(),
+            class_id=detections_dataframe['class_id'].to_numpy().ravel(),
+            tracker_id=detections_dataframe['id'].to_numpy().ravel(),
+            data={'class_name': detections_dataframe['class'].to_numpy().astype('<U10')}
+        )
 
-        point_list = [(int(x), int(y)) for x, y in zip(object_x, object_y)]
-        point_array = np.array(point_list)
-        point_array = point_array.reshape((-1, 1, 2))
+        # Dibujar etiquetas
+        object_labels = [f"{data['class_name']} {tracker_id} ({score:.2f})" for _, _, score, _, tracker_id, data in detections]
+        annotated_image = label_annotator.annotate(
+            scene=annotated_image,
+            detections=detections,
+            labels=object_labels )
         
-        image = cv2.polylines(
-            img=image,
-            pts=[point_array],
-            isClosed=False,
-            color=COLORS[object_class],
-            thickness=1 )
+        # Draw boxes
+        annotated_image = bounding_box_annotator.annotate(
+            scene=annotated_image,
+            detections=detections )
         
-        object_number += 1
+        # Draw tracks
+        if detections.tracker_id is not None:
+            annotated_image = trace_annotator.annotate(
+                scene=annotated_image,
+                detections=detections )
         
-    cv2.imshow("Resultado", image)
-    cv2.waitKey(0)
+        frame_number += 1
+
+        annotated_image = sv.draw_line(scene=annotated_image, start=sv.Point(x=65, y=220), end=sv.Point(x=118, y=220), color=sv.Color.BLUE)
+        annotated_image = sv.draw_line(scene=annotated_image, start=sv.Point(x=118, y=220), end=sv.Point(x=173, y=220), color=sv.Color.GREEN)
+        
+        cv2.imshow("Resultado", annotated_image)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
     cv2.destroyAllWindows()
-
-    
 
 
 def main():
