4539e9bfa335207cc7df478f53c2e72fdc542616
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2024-05-07 21:45:28 -0500

    Update region_timer.py

--- region_timer.py
+++ region_timer.py
@@ -4,16 +4,19 @@ import supervision as sv
 import cv2
 import yaml
 import torch
+import numpy as np
 from pathlib import Path
 import itertools
 
-from imutils.video import WebcamVideoStream
+from imutils.video import WebcamVideoStream, FileVideoStream
 from imutils.video import FPS
 
+import config
 from tools.print_info import print_video_info, print_progress, step_message
-from tools.video_info import from_camera
+from tools.video_info import from_camera, from_video_path
 from tools.csv_sink import CSVSink
 from tools.timers import FPSBasedTimer, ClockBasedTimer
+from tools.general import load_zones
 
 # For debugging
 from icecream import ic
@@ -22,46 +25,106 @@ from icecream import ic
 def main(
     source: str,
     output: str,
+    weights: str,
+    class_filter: list[int],
+    image_size: int,
+    confidence: float,
     show_image: bool,
-    save_video: bool,
-    save_source: bool
+    save_csv: bool,
+    save_video: bool
 ) -> None:
     step_count = itertools.count(1)
     
+    # Initialize model
+    model = YOLO(weights)
+    step_message(next(step_count), f'{Path(weights).stem.upper()} Model Initialized')
+
     # Initialize video capture
-    stream_source = eval(source) if source.isnumeric() else source
-    cap = cv2.VideoCapture(stream_source)
+    cap = cv2.VideoCapture(source)
     if not cap.isOpened(): quit()
-    source_info = from_camera(cap)
+    source_info = from_video_path(cap)
     cap.release()
     step_message(next(step_count), 'Video Source Initialized')
     print_video_info(source, source_info)
 
     if show_image:
         scaled_width = 1280 if source_info.width > 1280 else source_info.width
-        k = int(scaled_width * source_info.height / source_info.width)
-        scaled_height = k if source_info.height > k else source_info.height
+        scaled_height = int(scaled_width * source_info.height / source_info.width)
+        scaled_height = scaled_height if source_info.height > scaled_height else source_info.height
+
+    step_message(next(step_count), f"Device: {'GPU' if torch.cuda.is_available() else 'CPU'}")
+
+    # Annotators
+    COLORS = sv.ColorPalette.from_hex(["#E6194B", "#3CB44B", "#FFE119", "#3C76D1"])
+    line_thickness = int(sv.calculate_optimal_line_thickness(resolution_wh=(source_info.width, source_info.height)) * 0.5)
+    text_scale = sv.calculate_optimal_text_scale(resolution_wh=(source_info.width, source_info.height)) * 0.5
+
+    label_annotator = sv.LabelAnnotator(text_scale=text_scale, text_padding=2, text_position=sv.Position.TOP_LEFT, text_thickness=line_thickness, color=COLORS)
+    bounding_box_annotator = sv.BoundingBoxAnnotator(thickness=line_thickness, color=COLORS)
+
+    # Regions
+    polygons = load_zones(file_path=f"{Path(source).parent}/{Path(source).stem}_zones.json")
+    regions = [ sv.PolygonZone(
+        polygon=polygon,
+        triggering_anchors=(sv.Position.CENTER,)) for polygon in polygons ]
 
     # Start video processing
     step_message(next(step_count), 'Start Video Processing')
-    vs = WebcamVideoStream(stream_source)
+    fvs = FileVideoStream(source)
     video_sink = sv.VideoSink(target_path=f"{output}.mp4", video_info=source_info)
-    source_sink = sv.VideoSink(target_path=f"{output}_source.mp4", video_info=source_info)
     
     frame_number = 0
-    vs.start()
+    fvs.start()
     fps = FPS().start()
-    with video_sink, source_sink:
-        while True:
-            image = vs.read()
+    with video_sink:
+        while fvs.more():
+            image = fvs.read()
+            if image is None:
+                print()
+                break
             annotated_image = image.copy()
-            # Processes
+            
+            # YOLOv8 inference
+            results = model.track(
+                source=image,
+                persist=True,
+                imgsz=image_size,
+                conf=confidence,
+                device='cuda' if torch.cuda.is_available() else 'cpu',
+                classes=class_filter,
+                retina_masks=True,
+                verbose=False
+            )[0]
+            detections = sv.Detections.from_ultralytics(results)
+            detections = detections.with_nms()
 
-            # if show_image or save_video:
-                # process to show
+            if show_image or save_video:
+                # Draw regions
+                for index, region in enumerate(regions):
+                    annotated_image = sv.draw_polygon(
+                        scene=annotated_image,
+                        polygon=region.polygon,
+                        color=COLORS.by_idx(index)
+                    )
+
+                    detections_in_zone = detections[region.trigger(detections=detections)]
+                    custom_color_lookup = np.full(detections_in_zone.class_id.shape, index)
+
+                    # Draw labels
+                    object_labels = [f"{data['class_name']} {tracker_id} ({score:.2f})" for _, _, score, _, tracker_id, data in detections_in_zone]
+                    annotated_image = label_annotator.annotate(
+                        scene=annotated_image,
+                        detections=detections_in_zone,
+                        labels=object_labels,
+                        custom_color_lookup=custom_color_lookup )
+
+                    # Draw boxes
+                    annotated_image = bounding_box_annotator.annotate(
+                        scene=annotated_image,
+                        detections=detections_in_zone,
+                        custom_color_lookup=custom_color_lookup )
                 
             if save_video: video_sink.write_frame(frame=annotated_image)
-            if save_source: source_sink.write_frame(frame=image)
 
             print_progress(frame_number, None)
             frame_number += 1
@@ -81,14 +144,18 @@ def main(
     step_message(next(step_count), f"FPS: {fps.fps():.2f}")
 
     cv2.destroyAllWindows()
-    vs.stop()
+    fvs.stop()
 
 
 if __name__ == "__main__":
     main(
-        source="",
-        output="",
-        show_image=True,
-        save_video=True,
-        save_source=True
+        source=f"{config.INPUT_FOLDER}/{config.INPUT_VIDEO}",
+        output=f"{config.INPUT_FOLDER}/{Path(config.INPUT_VIDEO).stem}_timer",
+        weights=f"{config.YOLOV9_FOLDER}/{config.YOLOV9_WEIGHTS}.pt",
+        class_filter=config.CLASS_FILTER,
+        image_size=config.IMAGE_SIZE,
+        confidence=config.CONFIDENCE,
+        show_image=config.SHOW_IMAGE,
+        save_csv=config.SAVE_CSV,
+        save_video=config.SAVE_VIDEO
     )
