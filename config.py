e4964e1dceaef6304915bb8064340d4722521475
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2024-05-07 19:45:47 -0500

    Update config.py

--- config.py
+++ config.py
@@ -8,12 +8,15 @@ ROOT = Path('D:/Data')
 # INPUT_FOLDER = ROOT / 'MARZO_2024' / 'ARS-Iguana'
 # INPUT_VIDEO = 'ARS-Iguana CAM2.mp4'
 
-INPUT_FOLDER = ROOT / 'MARZO_2024' / 'ARS-SanPedro'
-INPUT_VIDEO = 'ARS-Metroplus San Pedro CAM1.mp4'
+# INPUT_FOLDER = ROOT / 'MARZO_2024' / 'ARS-SanPedro'
+# INPUT_VIDEO = 'ARS-Metroplus San Pedro CAM1.mp4'
+
+INPUT_FOLDER = ROOT
+INPUT_VIDEO = 'C3_detenido.mp4'
 
 # Deep Learning model configuration
 YOLOV8_FOLDER = ROOT / 'models' / 'yolov8'
-YOLOV8_WEIGHTS = 'yolov8x_cf_v3'
+YOLOV8_WEIGHTS = 'yolov8m'
 YOLOV9_FOLDER = ROOT / 'models' / 'yolov9'
 YOLOV9_WEIGHTS = 'yolov9c_cf_v6'
 RTDETR_FOLDER = ROOT / 'models' / 'rt-detr'
