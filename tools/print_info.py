847c17c47962fdd930e80a6afdfc5381fc143674
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2024-05-10 17:18:01 -0500

    Update print_info.py

--- tools/print_info.py
+++ tools/print_info.py
@@ -46,13 +46,13 @@ def print_video_info(source: str, video_info: VideoInfo):
     text_length = 20 + max(len(source_name) , len(f"{video_info.width} x {video_info.height}"))
     
     # Print video information
-    print(f"\n{red('*'*text_length)}")
-    print(f"{green('Source Information'):^{text_length+9}}")
-    print(f"{bold('Source'):<29}{source_name}")
-    print(f"{bold('Size'):<29}{video_info.width} x {video_info.height}")
-    print(f"{bold('Total Frames'):<29}{video_info.total_frames}") if video_info.total_frames is not None else None
-    print(f"{bold('Frames Per Second'):<29}{video_info.fps:.2f}")
-    print(f"\n{red('*'*text_length)}\n")
+    print(f"\n{green('*'*text_length)}")
+    print(f"{blue('Información del Origen'):^{text_length+9}}")
+    print(f"{bold('Origen'):<29}{source_name}")
+    print(f"{bold('Tamaño'):<29}{video_info.width} x {video_info.height}")
+    print(f"{bold('Cuadros Totales'):<29}{video_info.total_frames}") if video_info.total_frames is not None else None
+    print(f"{bold('Cuadros Por Segundo'):<29}{video_info.fps:.2f}")
+    print(f"\n{green('*'*text_length)}\n")
     
 
 def print_progress(frame_number: int, total_frames: int):
@@ -71,15 +71,9 @@ def print_progress(frame_number: int, total_frames: int):
     print(f"\r{green(percentage)}{frame_progress:>{frame_text_length}}  ", end="", flush=True)
     
 
-def print_times(frame_number: int, source_info: VideoInfo, progress_times: dict):
-    total_frames = source_info.total_frames
+def print_times(frame_number: int, total_frames: int, progress_times: dict):
     capture_time = progress_times['capture_time']
     inference_time = progress_times['inference_time']
-    detections_time = progress_times['detections_time']
-    tracks_time = progress_times['tracks_time']
-    saving_time = progress_times['saving_time']
-    drawings_time = progress_times['drawings_time']
-    files_time = progress_times['files_time']
     frame_time = progress_times['frame_time']
     
     if total_frames is not None:
@@ -93,22 +87,10 @@ def print_times(frame_number: int, source_info: VideoInfo, progress_times: dict)
         
     frame_text_length = (2 * len(str(total_frames))) + 3
     if frame_number == 0:
-        print(f"\n{percentage_title}{bold('Frame'):>{frame_text_length+9}}{bold('Capture'):>22}{bold('Inference'):>22}{bold('Detections'):>22}{bold('Tracks'):>22}{bold('Saving'):>22}{bold('Drawings'):>22}{bold('Files'):>22}{bold('Total'):>22}")
-
-    print(
-        f"{green(percentage)}"
-        f"{frame_progress:>{frame_text_length}}  "
-        f"{1000*(capture_time):8.2f} ms  "
-        f"{1000*(inference_time):8.2f} ms  "
-        f"{1000*(detections_time):8.2f} ms  "
-        f"{1000*(tracks_time):8.2f} ms  "
-        f"{1000*(saving_time):8.2f} ms  "
-        f"{1000*(drawings_time):8.2f} ms  "
-        f"{1000*(files_time):8.2f} ms  "
-        f"{1000*(frame_time):8.2f} ms"
-    )
+        print(f"\n{percentage_title}{bold('Frame'):>{frame_text_length+9}}{bold('Capture'):>22}{bold('Inference'):>22}{bold('Total'):>22}")
+    print(f"\r{green(percentage)}{frame_progress:>{frame_text_length}}  {1000*(capture_time):8.2f} ms  {1000*(inference_time):8.2f} ms  {1000*(frame_time):8.2f} ms  ", end="", flush=True)
     
 
 def step_message(step: str = None, message: str = None):
     step_text = green(f"[{step}]") if step != "Error" else red(f"[{step}]")
-    print(f"{step_text} {message} ✅")
+    print(f"{step_text} {message}")
